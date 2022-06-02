f79f64c7a20ffda6696d83c91ebc4004c4274e3f
ramaarief <ramaarief45@gmail.com>
2022-06-02 10:35:54 +0700

    Create Migration

---
+++ database/migrations/2022_06_02_030530_create_barang.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarang extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('namaBarang');
+            $table->integer('harga');
+            $table->integer('stok');
+            $table->date('tglUpdate');
+            $table->bigInteger('supplier_id')->unsigned()->nullable();
+            $table->bigInteger('kategori_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('supplier_id')->references('id')->on('supplier')->onDelete('cascade');
+            $table->foreign('kategori_id')->references('id')->on('kategori')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
