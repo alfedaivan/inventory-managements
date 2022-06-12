ebe62c5ceb2831c285b2c689b4667758c5391991
unknown <isfan.adheltyo@gmail.com>
2022-06-12 15:18:41 +0700

    Pull Search Kategori

--- resources/views/pages/supplier/index.blade.php
+++ resources/views/pages/supplier/index.blade.php
@@ -19,8 +19,23 @@
         @endif
 
         <div class="card">
-            <div class="card-header">
-                <a href="{{ route('supplier.create') }}" class="btn btn-primary">Tambah Supplier</a>
+            <div class="card-header justify-content-between">
+              <a href="{{ route('supplier.create') }}" class="btn btn-primary">Tambah Supplier</a>
+              <div class="d-flex align-items-center">
+                <form action="/supplier" method="GET" style="margin-right: 10px">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search" name="search" value="{{ $request->search }}" style="border-top-right-radius: 0; border-bottom-right-radius: 0;">
+                        <div class="input-group-btn">
+                            <button type="submit" class="btn btn-primary" style="border-top-left-radius: 0; border-bottom-left-radius: 0;"><i class="fas fa-search"></i></button>
+                        </div>
+                    </div>
+                </form>
+                <a class="btn btn-primary px-1" href="{{url('/supplier')}}">
+                  <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z" />
+                  </svg>
+                </a>
+              </div>
             </div>
             <div class="card-body p-0">
                 <div class="table-responsive">
