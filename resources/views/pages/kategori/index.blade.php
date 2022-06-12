c31e9936b2208ef1aaced92bae8d2f9d2cdc3aac
unknown <isfan.adheltyo@gmail.com>
2022-06-12 15:50:49 +0700

    Add Fitur Tombol Refresh

--- resources/views/pages/kategori/index.blade.php
+++ resources/views/pages/kategori/index.blade.php
@@ -18,8 +18,8 @@
         <div class="card">
             <div class="card-header justify-content-between">
                 <a href="{{ route('kategori.create') }}" class="btn btn-primary">Tambah Kategori</a>
-                <div class="card-header-form">
-                    <form method="get">
+                <div class="d-flex align-items-center">
+                    <form method="get" style="margin-right: 10px">
                         <div class="input-group">
                             <input type="text" class="form-control" name="search" value="{{ $request->search }}" placeholder="Search" style="border-top-right-radius: 0; border-bottom-right-radius: 0;">
                             <div class="input-group-btn">
@@ -27,6 +27,11 @@
                             </div>
                         </div>
                     </form>
+                    <a class="btn btn-primary px-1" href="{{url('/kategori')}}">
+                        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                          <path fill="currentColor" d="M17.65,6.35C16.2,4.9 14.21,4 12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20C15.73,20 18.84,17.45 19.73,14H17.65C16.83,16.33 14.61,18 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6C13.66,6 15.14,6.69 16.22,7.78L13,11H20V4L17.65,6.35Z" />
+                        </svg>
+                    </a>
                 </div>
             </div>
             <div class="card-body p-0">
