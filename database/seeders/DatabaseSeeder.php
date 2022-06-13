69c8de184f310410c32d73551e2c1a7820e13ceb
alfeda ivan widiaputra <alfedaivan@gmail.com>
2022-06-13 13:29:42 +0700

    user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'id' => '1',
+            'nama' => 'admin',
+            'noTelepon' => 'admin',
+            'alamat' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin'), // password
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
     }
 }
